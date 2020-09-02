07034f5752ab7d57128b5afcc79edd4e4c452c4a
wolfe <wolfewf@163.com>
2020-09-02 18:06:13 +0800

    modify

--- src/main/java/com/wolfe/insurance/controller/RedisDemo1Controller.java
+++ src/main/java/com/wolfe/insurance/controller/RedisDemo1Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wolfe
  * @classname RedisDemo1Controller
  * @description 测试高并发扣库存，redis中存有订单总量key: stock=99,
- * 同时启动两个服务RedisDemo1Controller，端口为8080，8081，使用jmeter测试200并发，
+ * 同时启动两个服务RedisDemo1Controller，端口为8080，8081（启动参数配置：-Dserver.port=8081），使用jmeter测试200并发，
  * 结果发现两个服务出现了多次扣除同一个库存的情况，产生了超卖的情况
  * @date 2020/9/2 3:44 下午
  * @since 1.0.0
